aaee97e3b8789095a14e714f00eeecd678a453a9
anton <antononrails@gmail.com>
2015-12-03 00:30:40 +0300

    Formatting

--- src/background.js
+++ src/background.js
@@ -6,7 +6,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.sendMessage(tabs[0].id, {cmd: "changeMode"}, function(response) {
       console.log(response.result);
       chrome.storage.local.get('isPlaying', function (result) {
-        chrome.browserAction.setIcon({path: result.isPlaying ? "images/icons/pause/32.png" :  "images/icons/play/32.png"});
+        chrome.browserAction.setIcon({path: result.isPlaying ? "images/icons/pause/32.png" : "images/icons/play/32.png"});
         chrome.storage.local.set({'isPlaying': !result.isPlaying}, function() {});
       });
     });
--- src/content.js
+++ src/content.js
@@ -2,7 +2,7 @@
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.cmd == "changeMode") {
-     $("#gp_play").click();
+    $("#gp_play").click();
     sendResponse({result: "clicked"});
   }
 });
