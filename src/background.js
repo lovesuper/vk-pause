d17c8d4f58fcd1d16d91a336f73cb990bd88ea8e
anton <antononrails@gmail.com>
2015-12-03 00:16:59 +0300

    little refactoring

--- src/background.js
+++ src/background.js
@@ -3,20 +3,11 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   console.log("[VK-Pause] ext clicked!");
   chrome.tabs.query({url: "https://vk.com/*", lastFocusedWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {cmd: "stopIt!"}, function(response) {
+    chrome.tabs.sendMessage(tabs[0].id, {cmd: "changeMode"}, function(response) {
       console.log(response.result);
       chrome.storage.local.get('isPlaying', function (result) {
-        console.log(">>>>> " + result.isPlaying);
-        if (result.isPlaying) {
-          console.log('State: true');
-          chrome.browserAction.setIcon({path: "images/icons/pause/32.png"});
-        } else {
-          console.log('State: false');
-          chrome.browserAction.setIcon({path: "images/icons/play/32.png"});
-        }
-        chrome.storage.local.set({'isPlaying': !result.isPlaying}, function() {
-          console.log('State saved!');
-        });
+        chrome.browserAction.setIcon({path: result.isPlaying ? "images/icons/pause/32.png" :  "images/icons/play/32.png"});
+        chrome.storage.local.set({'isPlaying': !result.isPlaying}, function() {});
       });
     });
   });
--- src/content.js
+++ src/content.js
@@ -1,8 +1,8 @@
 // content script
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("VK-Pause requested")
-    if (request.cmd == "stopIt!")
-       $("#gp_play").click();
-      sendResponse({result: "ok. clicked"});
-  });
+  if (request.cmd == "changeMode") {
+     $("#gp_play").click();
+    sendResponse({result: "clicked"});
+  }
+});
