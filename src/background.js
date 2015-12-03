3675bb99ca91d1ae4d7be12a37007409f677a421
Anton Lovesuper <lovesuper@iHack-Anton.local>
2015-12-03 17:26:08 +0300

    New feature: syncronizing with html

--- src/background.js
+++ src/background.js
@@ -1,26 +1,52 @@
-// background script
-function startStop() {
+// var STATES = {
+//   playing: {value: 0},
+//   paused: {value: 1}
+// }
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  switchState();
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+  if (command == "switchState") {
+    switchState()
+  }
+});
+
+function switchIconState() {
+  chrome.storage.local.get('isPlaying', function (result) {
+    chrome.browserAction.setIcon(
+      { path: result.isPlaying ? "images/icons/pause/32.png" : "images/icons/play/32.png" }
+    );
+    // setPlayingState(result.isPlaying ? STATES.playing : STATES.paused)
+    chrome.storage.local.set({'isPlaying': !result.isPlaying}, function() {});
+  });
+}
+
+function switchState() {
   chrome.tabs.query({url: "*://vk.com/*", lastFocusedWindow: true }, function(tabs) {
     tabs.forEach(function(tab) {
       chrome.tabs.sendMessage(tab.id, {cmd: "changeMode"}, function(response) {
-      console.log(response.result);
-      chrome.storage.local.get('isPlaying', function (result) {
-        chrome.browserAction.setIcon({path: result.isPlaying ? "images/icons/pause/32.png" : "images/icons/play/32.png"});
-        chrome.storage.local.set({'isPlaying': !result.isPlaying}, function() {});
+        switchIconState();
       });
-    });
     });
   });
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log("[VK-Pause] ext clicked!");
-  startStop();
-});
-
-chrome.commands.onCommand.addListener(function(command) {
-  console.log('Command:', command);
-  if (command == "startStop") {
-    startStop()
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.stateSwitched == "yes") {
+    sendResponse({result: "done!"});
+    switchIconState();
   }
 });
+
+// setPlayingState(state) {
+//   switch (state) {
+//     case STATES.playing:
+//       chrome.browserAction.setIcon({ path:"images/icons/pause/32.png"})
+//       break;
+//     case STATES.paused:
+//       chrome.browserAction.setIcon({ path:"images/icons/play/32.png"})
+//       break;
+//   }
+// }
--- src/content.js
+++ src/content.js
@@ -1,8 +1,15 @@
-// content script
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.cmd == "changeMode") {
     $("#gp_play").click();
-    sendResponse({result: "clicked"});
+    sendResponse({result: "ok"});
   }
+});
+
+$(document).ready(function () {
+  $("#ac_play").click(function() {
+    console.log("play/stop clicked");
+    chrome.runtime.sendMessage({stateSwitched: "yes"}, function(response) {
+      console.log(response.result);
+    });
+  });
 });
