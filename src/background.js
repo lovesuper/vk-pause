f07a634a5e95f436b8848ded454c9530eff215a6
Anton Lovesuper <lovesuper@iHack-Anton.local>
2015-12-03 12:24:27 +0300

    Now stop working for all tabs

--- src/background.js
+++ src/background.js
@@ -1,12 +1,14 @@
 // background script
 function startStop() {
   chrome.tabs.query({url: "*://vk.com/*", lastFocusedWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {cmd: "changeMode"}, function(response) {
+    tabs.forEach(function(tab) {
+      chrome.tabs.sendMessage(tab.id, {cmd: "changeMode"}, function(response) {
       console.log(response.result);
       chrome.storage.local.get('isPlaying', function (result) {
         chrome.browserAction.setIcon({path: result.isPlaying ? "images/icons/pause/32.png" : "images/icons/play/32.png"});
         chrome.storage.local.set({'isPlaying': !result.isPlaying}, function() {});
       });
+    });
     });
   });
 }
