00e515ecef05b64c8ae279c487ab56baa137421f
anton <antononrails@gmail.com>
2015-12-03 22:03:43 +0300

    Some healthy refactoring + sync

--- src/background.js
+++ src/background.js
@@ -1,52 +1,42 @@
-// var STATES = {
-//   playing: {value: 0},
-//   paused: {value: 1}
-// }
-
 chrome.browserAction.onClicked.addListener(function(tab) {
+  console.log("<VK-Pause> Extension icon has been clicked");
   switchState();
 });
 
-chrome.commands.onCommand.addListener(function(command) {
-  if (command == "switchState") {
-    switchState()
+chrome.commands.onCommand.addListener(function(hotkey) {
+  if (hotkey == HOTKEYS.switchState.value) {
+    console.log("<VK-Pause> Extension hotkey 'switchState' has been pressed")
+    switchState();
   }
 });
 
-function switchIconState() {
-  chrome.storage.local.get('isPlaying', function (result) {
-    chrome.browserAction.setIcon(
-      { path: result.isPlaying ? "images/icons/pause/32.png" : "images/icons/play/32.png" }
-    );
-    // setPlayingState(result.isPlaying ? STATES.playing : STATES.paused)
-    chrome.storage.local.set({'isPlaying': !result.isPlaying}, function() {});
-  });
-}
-
 function switchState() {
-  chrome.tabs.query({url: "*://vk.com/*", lastFocusedWindow: true }, function(tabs) {
+  chrome.tabs.query({url: "*://vk.com/*", lastFocusedWindow: true}, function(tabs) {
     tabs.forEach(function(tab) {
-      chrome.tabs.sendMessage(tab.id, {cmd: "changeMode"}, function(response) {
-        switchIconState();
+      console.log("<VK-Pause> Sending command 'switchState' to tab " + tab.id);
+      chrome.tabs.sendMessage(tab.id, {cmd: COMMANDS.switchState.value}, function(response) {
+        console.log("<VK-Pause> Extension has recieved response from content script. State is '" + response.newState.value + "'");
+        switchIconState(response.newState);
       });
     });
   });
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.stateSwitched == "yes") {
-    sendResponse({result: "done!"});
-    switchIconState();
-  }
+  console.log("<VK-Pause> Background has recieved request. State is '" + request.state.value + "'");
+  switchIconState(request.state);
+  sendResponse({result: "ok"});
 });
 
-// setPlayingState(state) {
-//   switch (state) {
-//     case STATES.playing:
-//       chrome.browserAction.setIcon({ path:"images/icons/pause/32.png"})
-//       break;
-//     case STATES.paused:
-//       chrome.browserAction.setIcon({ path:"images/icons/play/32.png"})
-//       break;
-//   }
-// }
+function switchIconState(state) {
+  // chrome.storage.local.get('isPlaying', function (result) {
+  //   switchState();
+  //   chrome.storage.local.set({'isPlaying': !result.isPlaying}, function() {});
+  // });
+  var iconPath = "images/icons/play/32.png"
+  if(state.value == STATES.playing.value) {
+    iconPath = "images/icons/pause/32.png"
+  }
+  console.log("<VK-Pause> Changing icon to " + iconPath);
+  chrome.browserAction.setIcon({ path: iconPath });
+}
--- src/content.js
+++ src/content.js
@@ -1,15 +1,45 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.cmd == "changeMode") {
-    $("#gp_play").click();
-    sendResponse({result: "ok"});
+$(document).ready(function () {
+  console.log("<VK-Pause> Extension started on page")
+  if($("#ac_play").length) {
+    console.log("<VK-Pause> StartStop button was found")
+    $("#ac_play").click(function() {
+      console.log("<VK-Pause> StartStop button has been clicked")
+      var playingState = getPlayingState();
+      reportPlayingState(playingState);
+    });
+    var playingState = getPlayingState();
+    console.log("<VK-Pause> StartStop state is '" + playingState.value + "'");
+    reportPlayingState(playingState);
+  } else {
+    console.log("<VK-Pause> StartStop button was not found")
   }
 });
 
-$(document).ready(function () {
-  $("#ac_play").click(function() {
-    console.log("play/stop clicked");
-    chrome.runtime.sendMessage({stateSwitched: "yes"}, function(response) {
-      console.log(response.result);
-    });
+function reportPlayingState(playingState) {
+  console.log("<VK-Pause> Sending to Background script button state as '" + playingState.value + "'");
+  chrome.runtime.sendMessage({state: playingState}, function(response) {
+    console.log("<VK-Pause> Background script has answered with '" + response.result + "'");
   });
+}
+
+function getPlayingState() {
+  var btnClass = $("#ac_play").attr("class");
+  if(btnClass == "fl_l" || btnClass == "fl_l over") {
+    return STATES.paused;
+  } else if(btnClass == "fl_l playing" || btnClass == "fl_l over playing") {
+    return STATES.playing;
+  }
+
+  return STATES.unknown;
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log("<VK-Pause> Background message has recieved")
+  if (request.cmd == COMMANDS.switchState.value) {
+    console.log("<VK-Pause> Clicking on playStop button")
+    $("#ac_play").click();
+    var state = getPlayingState();
+    console.log("<VK-Pause> Sending back new state")
+    sendResponse({newState: state});
+  }
 });
