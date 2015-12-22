d410b67a481e40360f61f073127360928c065064
Anton Lovesuper <lovesuper@iHack-Anton.local>
2015-12-22 14:52:33 +0300

    some fixes

--- assemble-release.sh
+++ assemble-release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#/bin/sh
 P=`jq -r '.short_name' manifest.json`
 V=`jq -r '.version' manifest.json`
 D=$P$V-publ
@@ -8,15 +8,20 @@ mkdir -p $D
 minify src/background.js
 minify src/content.js
 minify lib/constants.js
+minify lib/jquery-2.1.4.js
 
 find . -name '*.min.js' -o -name "*.png" | cpio -pdm $D
-
-json-minify manifest.json > $D/manifest.json
 
 rm src/background.min.js
 rm src/content.min.js
 rm lib/constants.min.js
 
-jq -R 'gsub(".js"; ".min.js")' $D/manifest.json --raw-output >> $D/manifest.json
+jq -R 'gsub("[.]js"; ".min.js")' manifest.json --raw-output > $D/manifest.json.tmp
+json-minify $D/manifest.json.tmp > $D/manifest.json
+
+rm $D/manifest.json.tmp
+
+zip -r $D.zip $D
 
 echo "done"
+
