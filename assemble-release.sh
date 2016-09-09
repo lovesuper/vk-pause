8a7b697f768e34feeaded2ea61fb83934cef2a0c
lovesuper <antononrails@gmail.com>
2016-09-10 02:03:51 +0300

    problem fixed

--- assemble-release.sh
+++ assemble-release.sh
@@ -5,16 +5,17 @@ D=$P$V-publ
 rm -r $D
 mkdir -p $D
 
-minify src/background.js
-minify src/content.js
-minify lib/constants.js
-minify lib/jquery-2.1.4.js
+minify src/background.js > src/background.min.js
+minify src/content.js > src/content.min.js
+minify lib/constants.js > lib/constants.min.js
+minify lib/jquery-2.1.4.js > lib/jquery-2.1.4.min.js
 
 find . -name '*.min.js' -o -name "*.png" | cpio -pdm $D
 
 rm src/background.min.js
 rm src/content.min.js
 rm lib/constants.min.js
+rm lib/jquery-2.1.4.min.js
 
 jq -R 'gsub("[.]js"; ".min.js")' manifest.json --raw-output > $D/manifest.json.tmp
 json-minify $D/manifest.json.tmp > $D/manifest.json
