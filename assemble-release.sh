7529293d2233399aaa8c7bf190ead3775270cc89
lovesuper <antononrails@gmail.com>
2016-09-15 10:52:24 +0300

    assemble fixes

--- assemble-release.sh
+++ assemble-release.sh
@@ -8,12 +8,16 @@ mkdir -p $D
 minify src/background.js > src/background.min.js
 minify src/content.js > src/content.min.js
 minify src/constants.js > src/constants.min.js
+minify src/options.js > src/options.min.js
+htmlmin -o src/options.min.html src/options.html
 
-find . -name '*.min.js' -o -name "*.png" | cpio -pdm $D
+find . -name '*.min.js' -name '*.min.html' -o -name "*.png" | cpio -pdm $D
 
 rm src/background.min.js
 rm src/content.min.js
 rm src/constants.min.js
+rm src/options.min.js
+rm src/options.min.html
 
 jq -R 'gsub("[.]js"; ".min.js")' manifest.json --raw-output > $D/manifest.json.tmp
 json-minify $D/manifest.json.tmp > $D/manifest.json
