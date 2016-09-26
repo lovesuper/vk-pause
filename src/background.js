6a59df4537eec8676359221c70e0469814ebd76f
lovesuper <antononrails@gmail.com>
2016-09-27 00:41:01 +0300

    new fragile of perfect ticker

--- src/background.js
+++ src/background.js
@@ -1,4 +1,5 @@
-var intervals = [];
+var ix = 0;
+var intervalID;
 
 chrome.browserAction.onClicked.addListener(function(tab) { appIconClicked(); });
 
@@ -12,17 +13,29 @@ chrome.commands.onCommand.addListener(function(hotkey) {
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.titleChanged) {
-    console.log("title changed");
+    ix = 0;
+    window.clearInterval(intervalID);
+    chrome.browserAction.setBadgeText({"text":""});
     chrome.browserAction.setTitle({ "title": request.titleChanged });
+    var arr = request.titleChanged.split("");
+    intervalID = window.setInterval(function() {
+      var txtArr = arr.slice(ix, ix+3);
+      if (txtArr.length != 0) {
+        chrome.browserAction.setBadgeText({"text": txtArr.join("")});
+        chrome.browserAction.setBadgeBackgroundColor({color: "blue"});
+        ix += 1;
+      } else { ix = 0; }
+    }, 500);
   } else if (request.playButtonClicked) {
-    console.log("playpause event:", request.state);
-    chrome.browserAction.setIcon({ path: "images/icons/" + request.state + "/48.png" });
-    //chrome.storage.local.set({ "lastPlayedTabId" : sender.tab.id });
+    chrome.storage.local.get("lastPlayedTabId", function(result) {
+      if (result.lastPlayedTabId == sender.tab.id) {
+        chrome.browserAction.setIcon({ path: "images/icons/" + request.state + "/48.png" });
+      }
+    });
   }
 });
 
 function appIconClicked() {
-  console.log("App icon clicked");
   var currentTimestamp = (new Date()).getTime();
   var query = {"lastTimeClicked": null, "intervalMills": DEFAULT_INTERVAL_MILLS};
   chrome.storage.local.get(query, function(result) {
@@ -31,10 +44,8 @@ function appIconClicked() {
     }
 
     if (result.lastTimeClicked && currentTimestamp - result.lastTimeClicked < result.intervalMills) {
-      console.log("go next");
       performAction(COMMAND.setToNext);
     } else {
-      console.log("go to opposite state");
       performAction(COMMAND.setToOppositeState);
     }
 
@@ -54,18 +65,13 @@ function performAction(action) {
     chrome.storage.local.get("lastPlayedTabId", function(result) {
       tabs.forEach(function(tab) {
         if(tab.id == result.lastPlayedTabId) {
-          console.log("working with playing tab", tab.id);
-          console.log("performing action", action.value, "at", tab.id)
           if (action.value == COMMAND.setToNext.value){
-            console.log("action: next");
             chrome.tabs.sendMessage(tab.id, COMMAND.setToNext);
           } else {
-            console.log("action: opposite state");
             chrome.tabs.sendMessage(tab.id, COMMAND.setToOppositeState);
           }
           chrome.storage.local.set({ "lastPlayedTabId" : tab.id });
         } else {
-          console.log("sure what this tab is silent", tab.id);
           chrome.tabs.sendMessage(tab.id, COMMAND.setToPause);
           chrome.browserAction.setIcon({ path: "images/icons/playing/48.png" });
         }
@@ -75,29 +81,19 @@ function performAction(action) {
 }
 
 function newVkInstanceCreationCompleteListener(tabId, info, tab) {
-  chrome.browserAction.setBadgeText({"text":""});
-  var intervalID = window.setInterval(function() {
-      var states = ["&--", "-#&", "#*#", "#-&", "#-#"]
-      var colors = ["blue", "green", "gray", "green", "blue"]
-      ix = Math.floor((Math.random() * 5) + 1);
-      chrome.browserAction.setBadgeText({"text": states[ix - 1]});
-      chrome.browserAction.setBadgeBackgroundColor({color: colors[ix - 1]});
-  }, 200);
-  intervals.push(intervalID);
-
   chrome.browserAction.setBadgeBackgroundColor({color: "CornflowerBlue"});
   if(info.status == "complete" && tab.url.indexOf("vk.com") > -1) {
     chrome.browserAction.setIcon({ path: "images/icons/playing/48.png" });
     chrome.storage.local.set({ "lastPlayedTabId" : tab.id });
-    intervals.forEach(function(item){ window.clearInterval(item); })
-    intervals = [];
-
     chrome.tabs.onUpdated.removeListener(newVkInstanceCreationCompleteListener);
   }
 }
 
 chrome.tabs.onRemoved.addListener(function (tabid) {
   chrome.storage.local.get("lastPlayedTabId", function(result) {
+    window.clearInterval(intervalID);
+    ix = 0;
+    chrome.browserAction.setBadgeText({"text":""});
     if (result.lastPlayedTabId) {
       chrome.tabs.query({ url: URL.vk.value}, function(tabs) {
         chrome.browserAction.setIcon({ path: "images/icons/playing/48.png" });
--- src/content.js
+++ src/content.js
@@ -1,17 +1,13 @@
 (function() {
   var playButton = document.querySelector(".top_audio_player");
   var titleBar = document.querySelector(".top_audio_player_title_wrap");
-
   if(playButton) {
     new MutationObserver(function(mutations, observer) {
       if (mutations[0].target.className.includes("top_audio_player_title")) {
-        console.log("skipping trash event");
         return;
       }
-      console.log("needful event");
       var state = mutations[0].target.className.includes("top_audio_player_playing")
         ?  STATE.paused.value : STATE.playing.value;
-      console.log("Event raised:", state);
       chrome.runtime.sendMessage( { "state": state, "playButtonClicked": true} );
     }).observe(playButton, { subtree: true, attributes: true });
   }
@@ -23,28 +19,25 @@
       chrome.runtime.sendMessage( { "titleChanged": currentTitle } );
     }).observe(titleBar, { subtree: true, attributes: true });
   }
+
 })();
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.value == COMMAND.setToPause.value) {
-    console.log("set to paused mode");
     var playButton = document.querySelector(".top_audio_player_play");
     var classList = playButton.classList;
     if (classList.contains("top_audio_player_playing")) {
       playButton.click();
     }
   } else if (request.value == COMMAND.setToPlay.value) {
-    console.log("set to playing mode");
     var playButton = document.querySelector(".top_audio_player_play");
     var classList = playButton.classList;
     if (!classList.contains("top_audio_player_playing")) {
       playButton.click();
     }
   } else if (request.value == COMMAND.setToOppositeState.value) {
-    console.log("switch playing state");
     document.querySelector(".top_audio_player_play").click()
   } else if (request.value == COMMAND.setToNext.value) {
-    console.log("next song");
     document.querySelector(".top_audio_player_next").click()
   }
 
